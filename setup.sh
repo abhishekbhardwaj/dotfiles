776cccc1661e681d4489a06bd63979867866d9ef
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2025-07-22 23:23:34 -0700

    chore: add trippy to Homebrew package installation list

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ install_homebrew_and_packages() {
     packages=(
         zsh tmux curl stow zoxide bat ripgrep eza fzf llm
         zsh-autosuggestions zsh-syntax-highlighting ngrok thefuck yt-dlp
-        gcc make unzip gh
+        gcc make unzip gh trippy
     )
 
     for pkg in "${packages[@]}"; do
